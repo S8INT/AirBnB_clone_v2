2c7ed368a80fd2d010edb715462351341ffa9cb5
kateeba peter <petrkateeb4@gmail.com>
2023-12-21 04:46:01 -0800

    update

--- console.py
+++ console.py
@@ -218,9 +218,8 @@ class HBNBCommand(cmd.Cmd):
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
-
         print(print_list)
 
     def help_all(self):
